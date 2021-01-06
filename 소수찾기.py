dc6e7857b0a8ad18c1f66003a95cfee5a1b9695b
imjaein <dlawodls96@naver.com>
2021-01-06 21:58:50 +0900

    20210106 시작

---
+++ 소수찾기.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+
+def solution(numbers):
+    answer = 0
+    lists = []
+    isPrime = True
+
+    for i in range(1, len(numbers) + 1):
+        for j in permutations(numbers, i):
+            lists.append(int(''.join(j)))
+
+    for i in list(set(lists)):
+        if i < 2:
+            continue
+
+        isPrime = True
+
+        for j in range(2, i):
+            if i % j == 0:
+                isPrime = False
+                break
+
+        if isPrime == True:
+            answer += 1
+
+    return answer
