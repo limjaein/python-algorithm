fa1c93a05b59b14f71105965f334972fa0b3a6d9
imjaein <dlawodls96@naver.com>
2021-06-20 20:15:07 +0900

    20210620 commit

---
+++ 06_DP/병사배치하기.py
@@ -0,0 +1,21 @@
+
+def solution():
+    N = int(input())
+    soldiers = list(map(int, input().split()))
+    result = 0
+
+    # dp[i] = 0 ~ i 까지 내림차순을 만족하는 최대 부분 수열 길이
+    dp = [1] * N
+
+    for i in range(1, N):
+        for j in range(i):
+            if soldiers[j] > soldiers[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    for i in range(N):
+        result = max(result, dp[i])
+
+    print(N - result)
+
+solution()
+
---
+++ 07_그래프/플로이드.py
@@ -0,0 +1,22 @@
+
+n = int(input())
+m = int(input())
+busCharge = [[1e9] * n for _ in range(n)]
+
+for i in range(m):
+    a, b, c = list(map(int, input().split()))
+    busCharge[a-1][b-1] = min(busCharge[a-1][b-1], c)
+
+for i in range(n):
+    busCharge[i][i] = 0
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            busCharge[i][j] = min(busCharge[i][j], busCharge[i][k] + busCharge[k][j])
+
+for i in range(n):
+    row = ""
+    for j in range(n):
+        row += str(busCharge[i][j]) + " "
+    print(row)
