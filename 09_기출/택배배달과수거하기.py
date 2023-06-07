72bd952afed83155699b1d8c94c0147c41f704a5
limjaein <dlawodls96@naver.com>
2023-06-07 23:25:47 +0900

    Update 택배배달과수거하기.py

--- 09_기출/택배배달과수거하기.py
+++ 09_기출/택배배달과수거하기.py
@@ -1,50 +1,22 @@
-# 시간초과 4
-def getMaxDist(deliveries, pickups, n):
-    for i in reversed(range(n)):
-        if deliveries[i] != 0 or pickups[i] != 0:
-            return i + 1
-    return 0
-        
-        
+
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    max_dist = getMaxDist(deliveries, pickups, n)
+    del_cnt = 0
+    pickup_cnt = 0
 
-    while max_dist > 0:
-        truck = [cap, 0]
-        answer += max_dist * 2
-        dist = max_dist
+    for i in range(n-1, -1, -1):
+        cnt = 0
 
-        for i in reversed(range(dist)):
-            del_cnt = deliveries[i]
-            pickup_cnt = pickups[i]
+        # 배달/수거 수량 차감 -2 -4 /
+        del_cnt -= deliveries[i]
+        pickup_cnt -= pickups[i]
 
-            # 배달 수량이 있으면
-            if del_cnt > 0 and truck[0] > 0:
-                if del_cnt <= truck[0]:
-                    deliveries[i] -= del_cnt
-                    truck[0] -= del_cnt
-                else:
-                    deliveries[i] -= truck[0]
-                    truck[0] = 0
-
-            # 수거 수량이 있으면
-            if pickup_cnt > 0:
-                rest_cap = cap - truck[1]
-
-                if rest_cap >= pickup_cnt:
-                    truck[1] += pickup_cnt
-                    pickups[i] = 0
-                else:
-                    pickups[i] -= rest_cap
-                    truck[1] = cap
-
-            # 모두 끝난 집이면 인덱스 - 1
-            if deliveries[i] == 0 and pickups[i] == 0:
-                max_dist = getMaxDist(deliveries, pickups, max_dist)
-
-            # 배달을 마치고, 수거수량이 가득찬 경우 break
-            if truck[0] == 0 and truck[1] == cap:
-                break
+        # 더 이상 배달 불가할 경우 cap 을 채우고 이동 카운트를 늘림
+        while del_cnt < 0 or pickup_cnt < 0:
+            del_cnt += cap
+            pickup_cnt += cap
+            cnt += 1
+                
+        answer += (i + 1) * 2 * cnt
 
     return answer
