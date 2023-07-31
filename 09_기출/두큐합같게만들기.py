895b737db5ab77f919f8615a35ee3d88e5e4cee9
limjaein <dlawodls96@naver.com>
2023-07-31 21:31:12 +0900

    Update 두큐합같게만들기.py

--- 09_기출/두큐합같게만들기.py
+++ 09_기출/두큐합같게만들기.py
@@ -1,26 +1,29 @@
-# 리스트 슬라이싱 -> deque 더빠르다 -> 그래도 안됨 -> 인덱스 이용하니 된다
-#from collections import deque
-
-
 def solution(queue1, queue2):
-    answer = 0
-
-    if (sum(queue1) + sum(queue2)) % 2 != 0:
-        return -1
-
-    target_sum = (sum(queue1) + sum(queue2)) // 2
-    left_sum = sum(queue1)
-
-    i, j, l_q1, l_q2 = 0, 0, len(queue1), len(queue2)
-    while i < l_q1 + l_q2 and j < l_q2:
-        if left_sum < target_sum:
-            left_sum += queue2[j]
-            queue1.append(queue2[j])
-            j += 1
-        elif left_sum > target_sum:
-            left_sum -= queue1[i]
-            i += 1
+    total_queue = queue1 + queue2
+    
+    target = sum(total_queue) // 2
+    
+    l = 0
+    r = len(queue1) - 1
+    s = sum(queue1)
+    cnt = 0
+    
+    while True:
+        if s < target:
+            # 타겟보다 합이 작은데, 더 이동할 수 없을 때
+            if r == len(total_queue) - 1:
+                return -1
+            else:
+                r += 1
+                s += total_queue[r]
+        elif s == target:
+            return cnt
         else:
-            return i + j
-
-    return -1
+            if l <= r:
+                s -= total_queue[l]
+                l += 1
+            
+        cnt += 1
+    
+    
+    return cnt
